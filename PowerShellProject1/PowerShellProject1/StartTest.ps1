e58a1797050c93dbb38e3a0aded55e348415e73c
allanuk <30735988+allanuk@users.noreply.github.com>
2017-08-04 23:50:40 +0100

    Updated StartTest.ps1 in Azure Automation.

--- PowerShellProject1/PowerShellProject1/StartTest.ps1
+++ PowerShellProject1/PowerShellProject1/StartTest.ps1
@@ -1,17 +1,15 @@
-﻿#
+#
 # Start Test Script
 #
 workflow StartTest
 {
 
-Set-AzureSubscription -SubscriptionName 'Pay-As-You-Go'
-
 $RGName = 'RG_LN119437_Automation'
 $AutoAccName = 'LN119437'
-$pwd = Get-AzureRmAutomationVariable -Name 'GTMetrixAPI' -ResourceGroupName $RGNAme -AutomationAccountName $AutoAccName
-$user = Get-AzureRmAutomationVariable -Name 'GTMetrixUserName' -ResourceGroupName $RGNAme -AutomationAccountName $AutoAccName
+$pwdraw = Get-AutomationVariable -Name 'GTMetrixAPI'
+$user = Get-AutomationVariable -Name 'GTMetrixUserName'
 #$user = 'allan_lowe@outlook.com'
-#$pwd = ConvertTo-SecureString "560eabec26a01de14bd4823d4f2ed198" -AsPlainText -Force
+$pwd = ConvertTo-SecureString $pwdraw -AsPlainText -Force
 $cred = New-Object Management.Automation.PSCredential ($user, $pwd)
 $uri = 'https://gtmetrix.com/api/0.1/test'
 
@@ -19,5 +17,5 @@ $body = 'url=https://www.ltegroup.co.uk'
 
 $Result = Invoke-RestMethod -Uri $uri -cred $cred -Method Post -Body $body
 
-New-AzureRmAutomationVariable -Name 'ResultTestURL' -Value $Result.poll_state_url -Encrypted $false -ResourceGroupName $RGName -AutomationAccountName $AutoAccName
+New-AutomationVariable -Name 'ResultTestURL' -Value $Result.poll_state_url -Encrypted $false -AutomationAccountName $AutoAccName
 }
