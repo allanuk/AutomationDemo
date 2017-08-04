6602cb0da84db881b6b8fb7f4dd82c59f7899128
allanuk <30735988+allanuk@users.noreply.github.com>
2017-08-04 23:54:43 +0100

    Updated PollTest.ps1 in Azure Automation.

--- PowerShellProject1/PowerShellProject1/PollTest.ps1
+++ PowerShellProject1/PowerShellProject1/PollTest.ps1
@@ -4,19 +4,14 @@
 workflow PollTest
 {
 
-Set-AzureSubscription -SubscriptionName 'Pay-As-You-Go' 
-
 $RGName = 'RG_LN119437_Automation'
 $AutoAccName = 'LN119437'
-$pwd = Get-AzureRmAutomationVariable -Name 'GTMetrixAPI' -ResourceGroupName $RGNAme -AutomationAccountName $AutoAccName
-$user = Get-AzureRmAutomationVariable -Name 'GTMetrixUserName' -ResourceGroupName $RGNAme -AutomationAccountName $AutoAccName
+$pwdraw = Get-AzureAutomationVariable -Name 'GTMetrixAPI'
+$user = Get-AzureAutomationVariable -Name 'GTMetrixUserName'
 #$user = 'allan_lowe@outlook.com'
-#$pwd = ConvertTo-SecureString "560eabec26a01de14bd4823d4f2ed198" -AsPlainText -Force
+$pwd = ConvertTo-SecureString $pwdraw -AsPlainText -Force
 $cred = New-Object Management.Automation.PSCredential ($user, $pwd)
 $uri = 'https://gtmetrix.com/api/0.1/test/ePl01IQl'
-
-
-
 
 Invoke-RestMethod -Uri $uri -Method Get -cred $cred 
 }
