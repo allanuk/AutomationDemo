71185274f8666a86f3e3c82acd364dd6477569ef
Allan Lowe <allan_lowe@outlook.com>
2017-08-04 23:31:36 +0100

    test

--- PowerShellProject1/PowerShellProject1/PollTest.ps1
+++ PowerShellProject1/PowerShellProject1/PollTest.ps1
@@ -6,8 +6,8 @@ workflow PollTest
 
 $RGName = 'RG_LN119437_Automation'
 $AutoAccName = 'LN119437'
-$pwd = Get-AzureRmAutomationVariable -Name GTMetrixAPI -ResourceGroupName $RGNAme -AutomationAccountName $AutoAccName
-$user = Get-AzureRmAutomationVariable -Name GTMetrixUserName -ResourceGroupName $RGNAme -AutomationAccountName $AutoAccName
+$pwd = Get-AzureRmAutomationVariable -Name 'GTMetrixAPI' -ResourceGroupName $RGNAme -AutomationAccountName $AutoAccName
+$user = Get-AzureRmAutomationVariable -Name 'GTMetrixUserName' -ResourceGroupName $RGNAme -AutomationAccountName $AutoAccName
 #$user = 'allan_lowe@outlook.com'
 #$pwd = ConvertTo-SecureString "560eabec26a01de14bd4823d4f2ed198" -AsPlainText -Force
 $cred = New-Object Management.Automation.PSCredential ($user, $pwd)
