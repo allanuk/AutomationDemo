a3105ecca5dd88cae80693b668adb2c74eb10c3e
allanuk <30735988+allanuk@users.noreply.github.com>
2017-08-05 13:30:23 +0100

    Updated StartTest.ps1 in Azure Automation.

--- PowerShellProject1/PowerShellProject1/StartTest.ps1
+++ PowerShellProject1/PowerShellProject1/StartTest.ps1
@@ -4,20 +4,33 @@
 workflow StartTest
 {
 
-Select-AzureSubscription -SubscriptionId '3c596139-25d6-4a0b-844b-1d1170dbdb9d' -Default
+$Conn = Get-AutomationConnection -Name AzureRunAsConnection
+Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
+-ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+
+$AppID = 'ffa7cbb9-47b7-4ced-b9ca-6d224985c72e'
+$TennentID = 'e39a912a-088a-4eed-90a0-877107f8c3f0'
+$Thumbprint = 'MO6GWvGs0VNb5mdSh2mAskor9AxqMN7KawX+gDAeBG8='
+Login-AzureRmAccount -ServicePrincipal -ApplicationId $AppID -TenantId $TennentID -CertificateThumbprint $Thumbprint
 
 $RGName = 'RG_LN119437_Automation'
 $AutoAccName = 'LN119437'
-$pwdraw = Get-AutomationVariable -Name 'GTMetrixAPI'
-$user = Get-AutomationVariable -Name 'GTMetrixUserName'
+$pwd = Get-AzureRmAutomationVariable -Name 'GTMetrixAPI'  -ResourceGroupName $RGName -AutomationAccountName $AutoAccName
+$user = Get-AzureRmAutomationVariable -Name 'GTMetrixUserName'  -ResourceGroupName $RGName -AutomationAccountName $AutoAccName
+
+write-output $user.value
+write-output $pwd
+
 #$user = 'allan_lowe@outlook.com'
-$pwd = ConvertTo-SecureString $pwdraw -AsPlainText -Force
-$cred = New-Object Management.Automation.PSCredential ($user, $pwd)
+#$pwd = ConvertTo-SecureString $pwdraw -AsPlainText -Force
+$cred = New-Object Management.Automation.PSCredential ($user.value, $pwd.value)
 $uri = 'https://gtmetrix.com/api/0.1/test'
 
 $body = 'url=https://www.ltegroup.co.uk'
 
 $Result = Invoke-RestMethod -Uri $uri -cred $cred -Method Post -Body $body
 
-New-AzureAutomationVariable -Name 'ResultTestURL' -Value $Result.poll_state_url -Encrypted $false -AutomationAccountName $AutoAccName
+New-AzureRMAutomationVariable -Name 'ResultTestURL' -Value $Result.poll_state_url -Encrypted $false -AutomationAccountName $AutoAccName -ResourceGroupName $RGName
+
+
 }
